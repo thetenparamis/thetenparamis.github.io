6e8d53faca80756b861947d72a8e137fefb12ce0
Jonatan Anriquez <jachness@gmail.com>
2023-07-04 21:31:00 -0300

    minor

--- src/index.js
+++ src/index.js
@@ -124,7 +124,7 @@ class App extends Component {
       const element = document.createElement("a");
       const file = new Blob([JSON.stringify(design)], { type: 'json/plain' });
       element.href = URL.createObjectURL(file);
-      element.download = "boletin_design_" + new Date().toISOString() + ".json";
+      element.download = "boletin_plantilla_" + new Date().toISOString() + ".json";
       document.body.appendChild(element); // Required for this to work in FireFox
       element.click();
     })
