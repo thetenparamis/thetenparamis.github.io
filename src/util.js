ca1227e161964e6ea0105cc7be9f0083da3d8431
Jonatan Anriquez <jachness@gmail.com>
2023-07-12 19:51:08 -0300

    Added HTML Crush

--- src/util.js
+++ src/util.js
@@ -1,12 +1,18 @@
 import { encode } from 'html-entities';
+import { crush } from "html-crush";
 
 const htmlPostProcessing = (html) => {
   html = encodeHTMLEntities(html);
+  html = minifyHTML(html);
   return html;
 }
 
 const encodeHTMLEntities = (html) => {
   return encode(html, { mode: 'nonAsciiPrintableOnly', level: 'html4' });
 };
+
+const minifyHTML = (html) => {
+  return crush(html).result;
+}
 
 export { htmlPostProcessing };
